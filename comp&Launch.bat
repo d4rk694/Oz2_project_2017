22e6e04002e8165f9180114aa5e51fc6ffbb7084
Ricci Dorian <dorian.ricci@outlook.com>
2017-04-24 09:10:58 +0200

    upgrade of the launcher

--- comp&Launch.bat
+++ comp&Launch.bat
@@ -1,20 +1,30 @@
 @echo off
 
-set /p compile=Do you want to recompile the game?(Y/N):
-if /i %compile%==y goto compilation
-if /i %compile%==n goto launch
+:loop
+echo Choose an option.
+echo 1: delete ozf
+echo 2: recompile oz files
+echo 3: launch the game
+echo 4:exit the program
+set /p compile=What do you want to do?
+cls
+if /i %compile%==1 goto delete
+if /i %compile%==2 goto compilation
+if /i %compile%==3 goto launch
+if /i %compile%==4 goto stop
+
+
+:delete
+del *.ozf
+goto loop
 
 :compilation
-del *.ozf
 ozc -c *.oz
-
-set /p compile=Do you want to lauch the game?(Y/N):
-if /i %compile%==y goto launch
-if /i %compile%==n goto stop
+goto loop
 
 :launch
 ozengine Main.ozf
-
+goto loop
 
 :stop
 exit
